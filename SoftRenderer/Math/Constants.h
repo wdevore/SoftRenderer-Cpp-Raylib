2b58a14e673b1f8bb9e054c65931ab0bbc945dca
wdevore <william.devore@gmail.com>
2026-03-07 21:10:39 -0500

    Added DEGTORAD

--- SoftRenderer/Math/Constants.h
+++ SoftRenderer/Math/Constants.h
@@ -4,4 +4,6 @@ namespace Maths
 {
     static float EPSILON = 0.000001f;
     static float PI = 3.14159265359f;
+    /// @brief Multiplying by this number converts Degrees to Radians
+    static float DEGTORAD = 180.0f / PI; // 57.29577951308f;
 } // namespace Maths
