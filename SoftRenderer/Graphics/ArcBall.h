d2a773ba43a3cfff65d1ab1a07efeac7210a38aa
wdevore <william.devore@gmail.com>
2026-03-07 21:09:59 -0500

    Added more methods and basic camera movement.

--- SoftRenderer/Graphics/ArcBall.h
+++ SoftRenderer/Graphics/ArcBall.h
@@ -13,6 +13,10 @@ class ArcBall
     const static int NSEGS = 1 << LG_NSEGS;
 
 private:
+    // Canvas size
+    int width{};
+    int height{};
+
     float fRadius{};
     Quat4f qNow{};
     Quat4f qDown{};
@@ -28,11 +32,8 @@ private:
 
     bool bDragging{false};
 
-    // Canvas size
-    int width{};
-    int height{};
-
     Vector3f vBallMouse{};
+    Quat4f vBallPoint{};
     Quat4f q{};
     Quat4f qConj{};
     std::vector<Vector3f> pts{};
@@ -45,6 +46,7 @@ private:
 
     Point3f p1{};
     Point3f p2{};
+    Vector3f vWork{};
 
     // Ken's arcball must be based on 2D screen coords where the +y is upwards respectively.
     // However, SWT 2D screen coords are such that +y is downward.
@@ -54,9 +56,10 @@ private:
     bool bScreenYOrientation{true}; // default to GL, Y is upwards.
 
 public:
-    Vector3f vLos{};
-    Vector3f vUp{};
-    Vector3f vHorz{};
+    Vector3f vLos{};    // Forward/backward
+    Vector3f vUp{};     // Up/down
+    Vector3f vHorz{};   // Left/Right
+    Vector3f vTarget{}; // Point of focus
 
     // Position represented as a matrix
     Matrix4f translation{};
@@ -70,4 +73,66 @@ public:
 
     void initialize();
     void reset();
+
+    void Place(Vector3f v, float r);
+    void Resize(int width, int height);
+    void MapScreenCoords(int x, int y);
+
+    void Update();
+
+    /// @brief Map window coordinates to Arcball coords. Output is vBallMouse.
+    /// @param vMouse
+    void MapMouseToSphere(const Vector3f &vMouse);
+
+    /// @brief Construct a unit quaternion from two points on unit sphere.
+    /// @param vFrom
+    /// @param vTo
+    /// @return results is in "vBallPoint" property
+    void MapFromBallPoints(const Vector3f &vFrom, const Vector3f &vTo);
+
+    /// @brief Convert a unit quaternion to two points on unit sphere.
+    /// @param q
+    /// @param vArcFrom Modified output
+    /// @param vArcTo Modified output
+    void MapToBallPoints(const Quat4f &q, Vector3f &vArcFrom, Vector3f &vArcTo);
+
+    /// @brief Convert window coordinates to sphere coordinates.
+    /// @param vMouse
+    /// @return results is in vBallMouse
+    void MouseOnSphere(const Vector3f &vMouse);
+
+    void SetBScreenYOrientation(bool bScreenYOrientation) { this->bScreenYOrientation = bScreenYOrientation; }
+
+    /// @brief
+    /// @param vDir The direction to rotate to.
+    void LookInDirection(const Vector3f &vDir);
+
+    /// @brief The base is assumed to be the world origin.
+    /// @param vTarget where the camera is to look at.
+    void LookAt(const Vector3f &vTarget);
+
+    /// @brief The base is assumed to be the world origin.
+    /// @param vBase
+    /// @param vTarget
+    void LookAt(const Vector3f &vBase, const Vector3f &vTarget);
+
+    /// @brief The base is assumed to be the world origin.
+    /// @param xb
+    /// @param yb
+    /// @param zb
+    /// @param xt
+    /// @param yt
+    /// @param zt
+    void LookAt(float xb, float yb, float zb, float xt, float yt, float zt);
+
+    /// @brief Calulated Line of Sight and stores in "vLos"
+    void ComputeLOS();
+
+    Matrix4f &GetTransformMatrix4f();
+    /// @brief This method is generally for computing visibility which means the
+    ///        position returned must be in world-space.
+    /// @return
+    Point3f &GetWorldPosition();
+
+    void MoveCameraBase(float dx, float dy, float dz);
 };
